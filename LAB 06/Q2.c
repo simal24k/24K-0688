3264bd1a873fb28061d3da8ddc2be6c2138fe068
simal24k <simal.mpgss@gmail.com>
2024-10-09 19:44:18 +0500

    Update Q2.c

--- LAB 06/Q2.c
+++ LAB 06/Q2.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+int main() {
+int i;
+int round; //how many times the loop worked
+int input;
+printf("Enter a number: ");
+scanf("%d", &input);
+for (int i = 1; i <= input; i++) { 
+	if(input%i == 0){
+		round++;
+	}}
+	if(round==2){
+		printf("Prime");
+	}
+    else{
+    	printf("Not Prime");
+	}
+return 0;
+}
