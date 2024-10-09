a8066f35c9c91fa3e4aa464b2377a784318c89d4
simal24k <simal.mpgss@gmail.com>
2024-10-09 22:30:30 +0500

    Update Q3.c

--- LAB 06/Q3.c
+++ LAB 06/Q3.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int main(){
+    int num;
+    int x = 0, y = 1, next;
+    int round = 0; 
+
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    printf("Fibonacci series up to %d: ", num);
+    
+    for (int i = 1; x <= num; i++) {
+        printf("%d ", x);
+
+        if (num % i == 0) {
+            round++; 
+        }
+
+        next = x + y;
+        x = y;
+        y = next;
+    }
+
+    if (round == 2) {
+        printf("\n%d is Prime.\n", num);
+    } 
+	else {
+        printf("\n%d is Not Prime.\n", num);
+    }
+
+    return 0;
+}
