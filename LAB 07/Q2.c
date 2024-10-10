df8114f4faf98e3c6e2346bad09ba072e3a50e68
simal24k <simal.mpgss@gmail.com>
2024-10-10 14:37:19 +0500

    Update Q2.c

--- LAB 07/Q2.c
+++ LAB 07/Q2.c
@@ -7,28 +7,28 @@ int main() {
         scanf("%99s", &input[i]);
 
     for (i = 0; input[i] != '\0' ; i++) {
-    	if(input[i] =='A'){
+    	if(input[i] =='A'|| input[i] == 'a'){
     		roundA++;
 		}
-		if(input[i] =='E'){
+		if(input[i] =='E'|| input[i] == 'e'){
     		roundE++;
     		
 		}
-		if(input[i] =='I'){
+		if(input[i] =='I'|| input[i] == 'i'){
     		roundI++;
    
     		
 		}
-		if(input[i] =='O'){
+		if(input[i] =='O'|| input[i] == 'o'){
     		roundO++;
 
 		}
-		if(input[i] =='U'){
+		if(input[i] =='U' || input[i] == 'u'){
     		roundU++;
 
     		
 		}}
-	printf("A = %d E = %d I = %d O = %d U = %d", roundA, roundE, roundI, roundO, roundU);
+	printf("a = %d e = %d i = %d o = %d u = %d", roundA, roundE, roundI, roundO, roundU);
 return 0;
     }
 
