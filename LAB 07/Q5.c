30c64d9a11f547dd08af25f74755a26533d3436e
simal24k <simal.mpgss@gmail.com>
2024-10-10 15:26:07 +0500

    Update Q5.c

--- LAB 07/Q5.c
+++ LAB 07/Q5.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int main() {
+    int input[100]; 
+    int i, temp;
+    int array2;
+    do {
+    	printf("Enter number greater than 0 (to stop enter input less than 0):\n");
+        scanf("%d", &input[i]);
+        if (input[i] < 0){
+            break;
+        }
+    }
+    while(input[i] > -1);
+    
+    for (i = 0; i < 3; i++) {
+    	temp = input[i];
+        input[i] = input[5-i];
+		input[5-i] = temp;
+		    }
+    array2 = temp;
+    if(input[i] == array2){
+    	printf("It is a Palindrome");
+	}
+	else{
+		printf("It is not a palindrome");
+	}
+    return 0;
+}
