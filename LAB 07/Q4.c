170bf3fb4eae2751a6fbda14c126bb709d4ea2f9
simal24k <simal.mpgss@gmail.com>
2024-10-25 23:41:11 +0500

    Update Q4.c

--- LAB 07/Q4.c
+++ LAB 07/Q4.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main() {
+    char str[100];
+    char *startPtr, *endPtr, tempChar;
+    int len = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    startPtr = str;
+    while (*(startPtr + len) != '\0') {
+        len++;
+    }
+    printf("Length: %d\n", len);
+
+    endPtr = str + len - 1;
+
+    while (startPtr < endPtr) {
+        tempChar = *startPtr;
+        *startPtr = *endPtr;
+        *endPtr = tempChar;
+
+        startPtr++;
+        endPtr--;
+    }
+
+    printf("Reversed string: %s\n", str);
+
+    return 0;
+}
