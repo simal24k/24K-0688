c479a74b266e9d9299af1e34c609ae290dc2736b
simal24k <simal.mpgss@gmail.com>
2024-11-10 01:20:22 +0500

    Update Q3.c

--- LAB 09/Q3.c
+++ LAB 09/Q3.c
@@ -1,37 +1,4 @@
-#include<stdio.h>
 
-void Prime_check(int num, int array[num]);
-int main(){
-  int i, num;
-  int array[num];
-
-  printf("Number of inputs: ");
-  scanf("%d", &num);
-  for(i=0;i<num;i++){
-  printf("\nEnter integer %d: ",i+1);
-  scanf("%d", &array[i]);}	
-   Prime_check(num, array);
-   return 0;
-}
-
-void Prime_check(int num, int array[num]){
-	int i,j;
-	int num;
-	for(j=0;j<num;j++){
-		int round = 0;
-	for ( i = 1; i <= array[j]; i++) { 
-	if(array[j]%i == 0){
-		round++;
-	}}
-	if(round==2){
-		printf("\n%d is Prime",array[j]);
-	}
-    else{
-    	printf("\n%d is Not Prime", array[j]);
-	}
-		
-	}}
-**********************
 #include <stdio.h>
 
 void Prime_check(int num, int array[num]);
