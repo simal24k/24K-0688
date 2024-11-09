8bb8e30defc26bd31bb8203454a820bb39234534
simal24k <simal.mpgss@gmail.com>
2024-11-10 02:24:49 +0500

    Update Q2.c

--- LAB 09/Q2.c
+++ LAB 09/Q2.c
@@ -1,24 +1,28 @@
+#include <stdio.h>
+
 void swapIntegers(int array[2]);
-int main(){
-  int array[2];
-  int i;
-  for(i=0;i<2;i++){
-  printf("\nEnter integer %d: ",i+1);
-  scanf("%d", &array[i]);}	
-   swapIntegers(array);
-   return 0;
+
+int main() {
+    int array[2];
+    int i;
+
+    for (i = 0; i < 2; i++) {
+        printf("\nEnter integer %d: ", i + 1);
+        scanf("%d", &array[i]);
+    }
+
+    swapIntegers(array);
+    return 0;
 }
 
-void swapIntegers(int array[2]){
-	int i;
-	int temp;
-	temp = array[0];
+void swapIntegers(int array[2]) {
+    int x; // Replaced temp with x
+    x = array[0];
     array[0] = array[1];
-    array[1] = temp;
-	
+    array[1] = x;
+
     printf("\nSwapped integers:\n");
-    for (i = 0; i < 2; i++) {
+    for (int i = 0; i < 2; i++) {
         printf("Number %d: %d\n", i + 1, array[i]);
-		 }
-		
-	}
+    }
+}
