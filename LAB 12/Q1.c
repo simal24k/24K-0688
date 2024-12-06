e1a83082253950b7f2f57eca343002d8ccc1edfc
simal24k <simal.mpgss@gmail.com>
2024-12-07 00:49:12 +0500

    Update Q1.c

--- LAB 12/Q1.c
+++ LAB 12/Q1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n;
+    int sum=0;
+    printf("how many number you want to sum:\n");
+    scanf("%d",&n);
+    int* pointer=(int*)malloc(sizeof(int)*n);
+    if(pointer==NULL){
+        printf("memory not allocated");
+        return 1;
+    }
+    for(int i=0;i<n;i++){
+        printf("enter the number %d:\n",i+1);
+        scanf("%d",&pointer[i]);
+        sum+=pointer[i];
 
+    }
+    printf("your sum is %d",sum);
+    free(pointer);
+    return 0;
+    }
